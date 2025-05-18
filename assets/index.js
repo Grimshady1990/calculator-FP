1bc974b5ffcbf6b394f703c18318ff050e21efc0
Grimshady1990 <frankiedthomas@gmail.com>
2025-05-18 20:30:50 +0100

    Create the functions needed to execute maths operations

---
+++ assets/index.js
@@ -0,0 +1,37 @@
+// CREATE a function called `add()` that takes two integers as parameters.
+function add(n1, n2) {
+// CREATE functionality that takes the two parameters and adds them together.
+// RETURN the results in the console.
+    let total = n1 + n2;
+    console.log(total);    
+} 
+
+// CREATE a function called `sub()` that takes two integers as parameters.
+function sub(n1, n2) {
+// CREATE functionality that takes the two parameters and subtracts them.
+// RETURN the results in the console.
+    let total = n1 - n2;
+    console.log(total);
+}
+
+// CREATE a function called `multi()` that takes two integers as parameters.
+function multi(n1, n2) {
+// CREATE functionality that takes the two parameters and multiplies them together.
+// RETURN the results in the console.
+    let total = n1 * n2;
+    console.log(total);
+}
+
+// CREATE a function called `div()` that takes two integers as parameters.
+function div(n1, n2) {
+// CREATE functionality that takes the two parameters and divides them.
+// RETURN the results in the console.
+    let total = n1 / n2;
+    console.log(total);
+}
+
+// CALL functions
+add(200, 200);
+sub(1000, 600);
+multi(80, 5);
+div(1200, 3);
