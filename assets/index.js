e9bc3f290c1b9a227c61c6c42282670e8b18827d
Grimshady1990 <frankiedthomas@gmail.com>
2025-05-19 23:10:01 +0100

    Complete the basic functionality of the calculator
    It can add, subtract, divide and multiply, and the numbers are locked so they don't trail off the display

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,56 @@
+let showDigits = document.querySelector("#display-text");
+
+const butTwo = document.querySelector("#two");
+butTwo.addEventListener("click", buttonPush2);
+
+const butThree = document.querySelector("#three");
+butThree.addEventListener("click", buttonPush3);
+
+const butFour = document.querySelector("#four");
+butFour.addEventListener("click", buttonPush4);
+
+const butFive = document.querySelector("#five");
+butFive.addEventListener("click", buttonPush5);
+
+const butSix = document.querySelector("#six");
+butSix.addEventListener("click", buttonPush6);
+
+const butSeven = document.querySelector("#seven");
+butSeven.addEventListener("click", buttonPush7);
+
+const butEight = document.querySelector("#eight");
+butEight.addEventListener("click", buttonPush8);
+
+const butNine = document.querySelector("#nine");
+butNine.addEventListener("click", buttonPush9);
+
+const butZero = document.querySelector("#zero");
+butZero.addEventListener("click", buttonPush0);
+
+const butCancel = document.querySelector("#cancel");
+butCancel.addEventListener("click", buttonPushC);
+
+/* const butDecimal = document.querySelector("#decimal");
+butOne.addEventListener("click", buttonPushD); */
+
+const butAdd = document.querySelector("#add");
+butAdd.addEventListener("click", buttonPushAdd);
+
+const butSub = document.querySelector("#sub");
+butSub.addEventListener("click", buttonPushSub);
+
+const butMulti = document.querySelector("#multi");
+butMulti.addEventListener("click", buttonPushMulti);
+
+const butDivide = document.querySelector("#divide");
+butDivide.addEventListener("click", buttonPushDivide);
+
+const butEquals = document.querySelector("#equals");
+butEquals.addEventListener("click", buttonPushEquals);
+
+
+let displayLength = showDigits.textContent.length;
+
 // CREATE a variable called `numOne` initialized to null.
 let numOne = null;
    
@@ -37,8 +90,8 @@ function operate() {
 function add(n1, n2) {
 // CREATE functionality that takes the two parameters and adds them together.
 // RETURN the results in the console.
-    let total = n1 + n2;
-    console.log(total);    
+    let total = +n1 + +n2;
+    showDigits.textContent = total.toString().slice(0, 9);    
 } 
 
 // CREATE a function called `sub()` that takes two integers as parameters.
@@ -46,7 +99,7 @@ function sub(n1, n2) {
 // CREATE functionality that takes the two parameters and subtracts them.
 // RETURN the results in the console.
     let total = n1 - n2;
-    console.log(total);
+    showDigits.textContent = total.toString().slice(0, 9);
 }
 
 // CREATE a function called `multi()` that takes two integers as parameters.
@@ -54,7 +107,7 @@ function multi(n1, n2) {
 // CREATE functionality that takes the two parameters and multiplies them together.
 // RETURN the results in the console.
     let total = n1 * n2;
-    console.log(total);
+    showDigits.textContent = total.toString().slice(0, 9);
 }
 
 // CREATE a function called `div()` that takes two integers as parameters.
@@ -62,19 +115,17 @@ function div(n1, n2) {
 // CREATE functionality that takes the two parameters and divides them.
 // RETURN the results in the console.
     let total = n1 / n2;
-    console.log(total);
+    showDigits.textContent = total.toString().slice(0, 9);
 }
 
-let showDigits = document.querySelector("#display-text");
+
 
 
 const butOne = document.querySelector("#one");
-butOne.addEventListener("click", buttonPush);
+butOne.addEventListener("click", buttonPush1);
 
-let displayLength = showDigits.textContent.length;
-
-
-function buttonPush(){
+function buttonPush1(){
+    console.log(displayLength);
     if (displayLength < 9) {
     showDigits.textContent += 1;
     displayLength++; 
@@ -85,9 +136,159 @@ function buttonPush(){
     
 }
 
-// CALL functions
-add(200, 200);
-sub(1000, 600);
-multi(80, 5);
-div(1200, 3);
-operate();
+
+
+
+
+
+function buttonPush2(){
+    if (displayLength < 9) {
+    showDigits.textContent += 2;
+    displayLength++; 
+    }
+    else {
+        return;
+    }
+    
+}
+
+function buttonPush3(){
+    if (displayLength < 9) {
+    showDigits.textContent += 3;
+    displayLength++; 
+    }
+    else {
+        return;
+    }
+    
+}
+
+function buttonPush4(){
+    if (displayLength < 9) {
+    showDigits.textContent += 4;
+    displayLength++; 
+    }
+    else {
+        return;
+    }
+    
+}
+
+function buttonPush5(){
+    if (displayLength < 9) {
+    showDigits.textContent += 5;
+    displayLength++; 
+    }
+    else {
+        return;
+    }
+    
+}
+
+function buttonPush6(){
+    if (displayLength < 9) {
+    showDigits.textContent += 6;
+    displayLength++; 
+    }
+    else {
+        return;
+    }
+    
+}
+
+function buttonPush7(){
+    if (displayLength < 9) {
+    showDigits.textContent += 7;
+    displayLength++; 
+    }
+    else {
+        return;
+    }
+    
+}
+
+function buttonPush8(){
+    if (displayLength < 9) {
+    showDigits.textContent += 8;
+    displayLength++; 
+    }
+    else {
+        return;
+    }
+    
+}
+
+function buttonPush9(){
+    if (displayLength < 9) {
+    showDigits.textContent += 9;
+    displayLength++; 
+    }
+    else {
+        return;
+    }
+    
+}
+
+function buttonPush0(){
+    if (displayLength < 9) {
+    showDigits.textContent += 0;
+    displayLength++; 
+    }
+    else {
+        return;
+    }
+    
+}
+
+function buttonPushC(){
+    
+    showDigits.textContent = "";
+    displayLength = 0;
+}
+
+/*function buttonPushD(){
+    if (displayLength < 9) {
+    showDigits.textContent += 1;
+    displayLength++; 
+    }
+    else {
+        return;
+    }
+    */
+
+function buttonPushAdd() {
+    numOne = showDigits.textContent;
+    operator = "+"
+    showDigits.textContent = "";
+    displayLength = 0;
+    console.log(numOne);
+}
+
+function buttonPushSub() {
+    numOne = showDigits.textContent;
+    operator = "-"
+    showDigits.textContent = "";
+    displayLength = 0;
+    console.log(numOne);
+}
+
+function buttonPushMulti() {
+    numOne = showDigits.textContent;
+    operator = "*"
+    showDigits.textContent = "";
+    displayLength = 0;
+    console.log(numOne);
+}
+
+function buttonPushDivide() {
+    numOne = showDigits.textContent;
+    operator = "/"
+    showDigits.textContent = "";
+    displayLength = 0;
+    console.log(numOne);
+}
+
+function buttonPushEquals() {
+    numTwo = showDigits.textContent;
+    operate();
+}
